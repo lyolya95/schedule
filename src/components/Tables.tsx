8f24db2d2e063b61287a62b72fec58c7389d85bb
Nazar Ashirov <newnewqwes@gmail.com>
2020-08-27 22:49:01 +0300

    feat: change table

--- src/components/Tables.tsx
+++ src/components/Tables.tsx
@@ -1,11 +1,83 @@
 import React from "react";
 import { TablesProps } from "./TablesProps.model";
-import { TablesTemp } from "./TablesTemp";
+import { Table, Tag, Space } from "antd";
 
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+    render: (text: string) => <a>{text}</a>,
+  },
+  {
+    title: "Age",
+    dataIndex: "age",
+    key: "age",
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+    key: "address",
+  },
+  {
+    title: "Tags",
+    key: "tags",
+    dataIndex: "tags",
+    render: (tags: any) => (
+      <>
+        {tags.map((tag: any) => {
+          let color = tag.length > 5 ? "geekblue" : "green";
+          if (tag === "loser") {
+            color = "volcano";
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+    ),
+  },
+  {
+    title: "Action",
+    key: "action",
+    render: (text: any, record: any) => (
+      <Space size="middle">
+        <a>Invite {record.name}</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+];
+
+const data = [
+  {
+    key: "1",
+    name: "John Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park",
+    tags: ["nice", "developer"],
+  },
+  {
+    key: "2",
+    name: "Jim Green",
+    age: 42,
+    address: "London No. 1 Lake Park",
+    tags: ["loser"],
+  },
+  {
+    key: "3",
+    name: "Joe Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+];
 export const Tables: React.FC<TablesProps> = () => {
   return (
     <div>
-      <TablesTemp />
+      <Table columns={columns} dataSource={data} />
     </div>
   );
 };
--- src/components/dist/Tables.js
+++ src/components/dist/Tables.js
@@ -2,8 +2,70 @@
 exports.__esModule = true;
 exports.Tables = void 0;
 var react_1 = require("react");
-var TablesTemp_1 = require("./TablesTemp");
+var antd_1 = require("antd");
+var columns = [
+    {
+        title: "Name",
+        dataIndex: "name",
+        key: "name",
+        render: function (text) { return react_1["default"].createElement("a", null, text); }
+    },
+    {
+        title: "Age",
+        dataIndex: "age",
+        key: "age"
+    },
+    {
+        title: "Address",
+        dataIndex: "address",
+        key: "address"
+    },
+    {
+        title: "Tags",
+        key: "tags",
+        dataIndex: "tags",
+        render: function (tags) { return (react_1["default"].createElement(react_1["default"].Fragment, null, tags.map(function (tag) {
+            var color = tag.length > 5 ? "geekblue" : "green";
+            if (tag === "loser") {
+                color = "volcano";
+            }
+            return (react_1["default"].createElement(antd_1.Tag, { color: color, key: tag }, tag.toUpperCase()));
+        }))); }
+    },
+    {
+        title: "Action",
+        key: "action",
+        render: function (text, record) { return (react_1["default"].createElement(antd_1.Space, { size: "middle" },
+            react_1["default"].createElement("a", null,
+                "Invite ",
+                record.name),
+            react_1["default"].createElement("a", null, "Delete"))); }
+    },
+];
+var data = [
+    {
+        key: "1",
+        name: "John Brown",
+        age: 32,
+        address: "New York No. 1 Lake Park",
+        tags: ["nice", "developer"]
+    },
+    {
+        key: "2",
+        name: "Jim Green",
+        age: 42,
+        address: "London No. 1 Lake Park",
+        tags: ["loser"]
+    },
+    {
+        key: "3",
+        name: "Joe Black",
+        age: 32,
+        address: "Sidney No. 1 Lake Park",
+        tags: ["cool", "teacher"]
+    },
+];
 exports.Tables = function () {
     return (react_1["default"].createElement("div", null,
-        react_1["default"].createElement(TablesTemp_1.TablesTemp, null)));
+        react_1["default"].createElement(antd_1.Table, { columns: columns, dataSource: data })));
 };
