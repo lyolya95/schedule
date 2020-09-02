4a8c7256de7ff63870bee7c8f8919a1afebdf0d7
Ольга Бугрова <bugrovaoa@it2g.ru>
2020-09-02 22:52:42 +0400

    feat: add component calendar

---
+++ src/components/Calendar/CalendarItem.tsx
@@ -0,0 +1,10 @@
+import { Calendar } from 'antd';
+import React from 'react';
+
+export const CalendarItem = () => {
+  return (
+    <div>
+      <Calendar />
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CalendarItem } from './components/Calendar/CalendarItem';
 
 ReactDOM.render(
   <React.StrictMode>
+    <CalendarItem />
   </React.StrictMode>,
   document.getElementById('root')
 );
