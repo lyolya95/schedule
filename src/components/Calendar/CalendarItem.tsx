c37c08c3daf8bae726baba4e112c873410141cf7
Ольга Бугрова <bugrovaoa@it2g.ru>
2020-09-05 21:23:38 +0400

    feat: add styles calendar

--- src/components/Calendar/CalendarItem.tsx
+++ src/components/Calendar/CalendarItem.tsx
@@ -1,59 +1,63 @@
 import { Badge, Calendar } from 'antd';
 import 'antd/dist/antd.css';
-import { CalendarMode } from 'antd/lib/calendar/generateCalendar';
 import moment from 'moment';
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { events } from '../../mocks/events';
+import { eventsTitleCalendar } from '../../mocks/eventsTitleCalendar';
 import './Calendar.scss';
 
 export const CalendarItem = () => {
-
-  const titleCourse = events.map((i) => i.course)[0];
-
-  const getListData = (value: moment.Moment) => {
-    let listData: {type: string, content: string}[] = [];
+  const titleCourse = useMemo(() => events.map((i) => i.course)[0], []);
+  const eventsTitle = useMemo(() => {
+    return eventsTitleCalendar.map((item: any) => (
+      <li key={item.content}>
+        <Badge status={item.type} text={item.content} />
+      </li>
+    ));
+  }, []);
+  const getListData = useCallback((value: moment.Moment) => {
+    let listData: { type: string; content: string }[] = [];
     events.map((i) =>
       i.events.map((i) => {
         const type = i.type === 'deadline' ? 'error' : i.type === 'task' ? 'success' : i.type === 'live' && 'warning';
         switch (value.date()) {
-          case +i.date.slice(0, 2): 
-            listData.push(
-              {
-                type: `${type}`,
-                content: i.name,
-              },
-            );
+          case +i.date.slice(0, 2):
+            listData.push({
+              type: `${type}`,
+              content: i.name,
+            });
             break;
           default:
         }
       })
     );
     return listData || [];
-  };
+  }, []);
 
-  const dateCellRender = (value: moment.Moment) => {
-    const listData = getListData(value);
-    return (
-      <ul className="events">
-        {listData.map((item: any) => (
-          <li key={item.content}>
-            <Badge status={item.type} text={item.content} />
-          </li>
-        ))}
-      </ul>
-    );
-  };
-  const onPanelChange = (value: moment.Moment, mode: CalendarMode) => {
-    console.log(value.format('YYYY-MM-DD'), mode);
-  };
+  const dateCellRender = useCallback(
+    (value: moment.Moment) => {
+      const listData = getListData(value);
+      return (
+        <ul className="events">
+          {listData.map((item: any) => (
+            <li key={item.content}>
+              <Badge status={item.type} text={item.content} />
+            </li>
+          ))}
+        </ul>
+      );
+    },
+    [getListData]
+  );
 
   return (
-    <div>
+    <div className="calendar">
       <div className="calendar-title">
         <span>Calendar</span>
         <span>{titleCourse}</span>
       </div>
-      <Calendar onPanelChange={onPanelChange} dateCellRender={dateCellRender} />
+      <ul className="events-title">{eventsTitle}</ul>
+      <Calendar dateCellRender={dateCellRender} />
     </div>
   );
 };
