f95c0f9c9881b63046a6c0b10a104356493a6a56
Ольга Бугрова <bugrovaoa@it2g.ru>
2020-09-16 00:32:38 +0400

    refactor: edit calendar, add tags

--- src/components/Calendar/CalendarItem.tsx
+++ src/components/Calendar/CalendarItem.tsx
@@ -1,8 +1,9 @@
-import { Badge, Calendar } from 'antd';
+import { Calendar, Tag } from 'antd';
 import 'antd/dist/antd.css';
 import moment from 'moment';
 import React, { FC, useCallback } from 'react';
 import { events } from '../../mocks/events';
+import { switchTypeToColor } from '../utilities/switcher';
 import './Calendar.scss';
 import { CalendarItemProps } from './CalendarItem.model';
 
@@ -11,11 +12,10 @@ export const CalendarItem: FC<CalendarItemProps> = React.memo(({ isShowCalendarO
     let listData: { type: string; content: string }[] = [];
     events.map((i) =>
       i.events.map((i) => {
-        const type = i.type === 'deadline' ? 'error' : i.type === 'task' ? 'success' : i.type === 'live' && 'warning';
         switch (value.date()) {
           case +i.date.slice(0, 2):
             listData.push({
-              type: `${type}`,
+              type: i.type,
               content: i.name,
             });
             break;
@@ -31,20 +31,24 @@ export const CalendarItem: FC<CalendarItemProps> = React.memo(({ isShowCalendarO
     (value: moment.Moment) => {
       const listData = getListData(value);
       return (
-        <ul className="events">
-          {listData.map((item: any) => (
-            <li key={item.content}>
-              <Badge status={item.type} text={item.content} />
-            </li>
-          ))}
-        </ul>
+        <>
+          <ul className="events">
+            {listData.map((item: any) => (
+              <li key={item.content}>
+                <Tag key={item.content} color={switchTypeToColor(item.type)} className="size">
+                  {item.content}
+                </Tag>
+              </li>
+            ))}
+          </ul>
+        </>
       );
     },
     [getListData]
   );
 
   return (
-    <div className="calendar">
+    <div className="calendar" style={{ width: '100px' }}>
       <Calendar dateCellRender={dateCellRender} />
     </div>
   );
